2b15ecb38dd492704219cd267edc3a6328e6af19
Yodra López <yodralopez@gmail.com>
2020-09-17 13:37:21 +0100

    update docker cheatsheet, add article of docker, update bio

--- components/bio/Bio.tsx
+++ components/bio/Bio.tsx
@@ -9,11 +9,12 @@ const Link = styled.a`
 export const Bio: FC = () => {
   return <div>
     <h2>Además de tomar 🍻 birras 🍻...</h2>
-    <p>En ocasiones pico algo de código aplicando el mito de las buenas prácticas y el gran desconocido testing.</p>
+    <p>En ocasiones pico algo de código aplicando el "mito" de las buenas prácticas y el gran desconocido testing.</p>
+    <p>Aunque mis inicios fuerón con <Link href="https://twitter.com/java" target="_blank">@java</Link> me considero agnóstica a los lenguajes de programación.</p>
     <p>Lo paso fatal trabajando desde casa en <Link href="https://twitter.com/canarias_es" target="_blank">@Canarias</Link> 🏝.</p>
-    <p>Intento dar a conocer estas prácticas tan extrañas como XP, TDD, Refactoring y otras, a la
+    <p>Intento dar a conocer estas prácticas tan extrañas como XP, TDD o Refactoring a la
       siguiente generación de developers, en cursos de FullStack y FrontEnd de <Link href="https://twitter.com/eoi" target="_blank">@EOI</Link>.</p>
-    <p>Y cómo me encanta esto de luchar contra corriente soy Co-organizadora en la comunidad de Mujeres
+    <p>Y cómo me encanta esto de luchar contra corriente soy Co-organizadora en la comunidad de mujeres
       desarrolladoras <Link href="https://twitter.com/adalovedev" target="_blank">@adalovedev</Link>. 💪</p>
     <p>También me gusta contar batallitas 🤺 como las abuelas 👵👵, así que de vez en cuando doy alguna charla que
       puedes encontrar en esta web.</p>
--- data/articles.ts
+++ data/articles.ts
@@ -9,10 +9,28 @@ export interface ArticleModel {
 
 export const articles: ArticleModel[] = [
   {
-    id: '1',
-    title: 'Cheat sheet: Dockerfile y docker-compose',
-    link: '/docker-cheatsheet.pdf',
+    id: '7',
+    title: 'Artículo: Docker + NodeJS + Buenas Prácticas',
+    link: 'https://softwarecrafters.io/devops/docker-nodejs-buenas-practicas',
+    image: 'https://images.unsplash.com/photo-1526085714137-8c2402e21151?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3302&q=80',
+  },
+  {
+    id: '5',
+    title: 'Tips: Oratoria',
+    link: 'https://leanmind.es/es/blog/taller-de-oratoria/',
+    image: '/img-tips-oratoria.png',
+  },
+  {
+    id: '6',
+    title: 'Cheat sheet: Dockerfile y docker-compose v2.0',
+    link: '/docker-cheatsheet-v2.pdf',
     image: '/img-docker.png',
+  },
+  {
+    id: '4',
+    title: 'Charla: Mira lo que ha hecho... GIT',
+    link:'https://www.youtube.com/embed/ZuaftLzA6WI',
+    image: 'https://live.staticflickr.com/65535/49157525538_19d5e2f23c_o.jpg'
   },
   {
     id: '2',
@@ -22,26 +40,14 @@ export const articles: ArticleModel[] = [
   },
   {
     id: '3',
-    title: 'Artículo: Testing de UI en Swift con Kif',
-    link:'https://leanmind.es/es/blog/testing_ui_swift_con_kif/',
-    image: 'https://images.unsplash.com/photo-1523726491678-bf852e717f6a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
-  },
-  {
-    id: '4',
-    title: 'Charla: Mira lo que ha hecho... GIT',
-    link:'https://www.youtube.com/embed/ZuaftLzA6WI',
-    image: 'https://live.staticflickr.com/65535/49157525538_19d5e2f23c_o.jpg'
-  },
-  {
-    id: '5',
     title: 'Artículo: Afrontando un MVP con impacto social',
     link: 'https://leanmind.es/es/blog/afrontar-mvp/',
     image: 'https://images.unsplash.com/photo-1509062522246-3755977927d7?ixlib=rb-1.2.1&auto=format&fit=crop&w=2604&q=80'
   },
   {
-    id: '6',
-    title: 'Tips: Oratoria',
-    link: 'https://leanmind.es/es/blog/taller-de-oratoria/',
-    image: '/img-tips-oratoria.png',
+    id: '1',
+    title: 'Artículo: Testing de UI en Swift con Kif',
+    link:'https://leanmind.es/es/blog/testing_ui_swift_con_kif/',
+    image: 'https://images.unsplash.com/photo-1523726491678-bf852e717f6a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
   }
 ];
